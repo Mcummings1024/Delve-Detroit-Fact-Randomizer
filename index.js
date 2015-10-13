60a7ccc3b6e974b7158ce1c2bbf59f186fbe7428
Matthew <Mattxsc@gmail.com>
2015-10-13 16:12:46 -0400

    adding request parameters

--- index.js
+++ index.js
@@ -20,14 +20,15 @@ app.set('port', (process.env.PORT || 5000));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log(req);
   res.send(lyrics[randomLyric()]);
 });
 
 // views is directory for all template files
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   console.log('hello there');
