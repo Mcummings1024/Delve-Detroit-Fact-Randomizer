2db12febd574b7e9a6b8c1ebc3d8fee6b2d1b68e
Matthew <Mattxsc@gmail.com>
2015-10-13 13:32:03 -0400

    fixing ejs bug

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
