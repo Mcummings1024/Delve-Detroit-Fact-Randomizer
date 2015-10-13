64569edf274c842da1eeefa699accde65bf0bd65
Matthew <Mattxsc@gmail.com>
2015-10-13 13:42:24 -0400

    testing lyric randomizer

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 
+var http = require('http');
+var song = require('./lyrics');
+
+var lyrics = song.lyrics;
+var randomLyric = song.randomize;
+
+function onRequest(request, response) {
+	response.writeHead(200, { "Content-type": "text/plain" });
+	response.write(lyrics[randomLyric()]);
+	response.end();
+}
+
 console.log('test');
 
 app.set('port', (process.env.PORT || 5000));
@@ -14,7 +26,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  console.log('hello there');
   response.render('pages/index');
+  response.write(lyrics[randomLyric()]);
 });
 
 app.listen(app.get('port'), function() {
