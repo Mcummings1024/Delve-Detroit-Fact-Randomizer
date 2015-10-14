1b43a2a96881a0c8cf82cad7d76b698304c61aa7
Matthew <Mattxsc@gmail.com>
2015-10-14 09:18:11 -0400

    working on parameters

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log(req.params.code);
   if(req.params.code === 1890) {
     res.send("1890");
   }
