29335f15a8918737f95963d2297063924e17e082
Matthew <Mattxsc@gmail.com>
2015-10-14 11:33:49 -0400

    fixing codes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if(req.params.code === 1890) {
+  if(req.path === "/?code=1890") {
     res.send(ninetiesFacts[randomize(ninetiesFacts)]);
   }
   else if(req.params.code === 1920) {
