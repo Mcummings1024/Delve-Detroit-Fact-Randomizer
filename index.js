1f3a39934a146c6cad13ff2319b4ca8d08839de5
Matthew <Mattxsc@gmail.com>
2015-10-14 09:22:36 -0400

    working on parameters

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   console.log(req.params.code);
-  if(req.params.code === 1890) {
+  if(req) {
     res.send("1890");
   }
   else {
